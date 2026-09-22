d7aa199aea20a7951d4b9eab8a2206edff968600
josesito-mx <jmiguelmeza0710@gmail.com>
2026-09-21 19:09:57 -0600

    new update filed oriented driving

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldOrientedDriving.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class FieldOrientedDriving extends OpMode {
+    MecanumD drive = new MecanumD();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFiledRelative(forward, strafe,rotate);
+
+    }
+}
